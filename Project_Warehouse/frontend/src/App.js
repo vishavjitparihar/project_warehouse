d7bb7a76d66db8b816749ef09021a40917caa134
vishavjitparihar <70875540+vishavjitparihar@users.noreply.github.com>
2022-10-04 20:13:24 -0700

    Basic Frontend

---
+++ Project_Warehouse/frontend/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="WelcomePage">
+      <header className="Welcome-header">
+        <img src={logo} className="Welcome-logo" alt="logo" />
+        <p>
+          Welcome to Apple's Warehoue Page.
+        </p>
+          <a
+            className="Sub-link"
+            href="https://www.apple.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Click here for our Offical Website
+          </a>
+          <br></br>
+          <a
+            className="Sub-link"
+            href="https://www.apple.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Continue to our Warehouse Website
+          </a>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
