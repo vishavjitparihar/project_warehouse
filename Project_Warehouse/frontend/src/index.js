c91123fcbd7cd935829e1e35ede7eeb529300659
vishavjitparihar <70875540+vishavjitparihar@users.noreply.github.com>
2022-10-04 20:52:41 -0700

    trimmed some extra css fat

--- Project_Warehouse/frontend/src/index.js
+++ Project_Warehouse/frontend/src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import WelcomePage from './WelcomePage';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <WelcomePage />
     <App />
   </React.StrictMode>
 );
+
